4fa283b2e34827ca795dbfbfb26837bc7b512860
riowp <prabowo.riowahyu@gmail.com>
2023-02-13 20:56:43 +0700

    debug

--- frontend/baseUrl.js
+++ frontend/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "https://daac-2001-448a-10a9-1a9e-394e-dfac-504c-74b2.ngrok.io"
+const baseUrl = "https://daac-2001-448a-10a9-1a9e-394e-dfac-504c-74b2.ngrok.io";
 
-export default baseUrl
+export default baseUrl;
